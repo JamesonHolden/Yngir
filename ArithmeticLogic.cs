60dfdd5c51eb2adb11ade3f1206dcd230e0fd99f
JamesonHolden <77400851+JamesonHolden@users.noreply.github.com>
2021-01-13 15:49:06 +0000

    Update ArithmeticLogic.cs

--- ArithmeticLogic.cs
+++ ArithmeticLogic.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unyschkovsky {
+namespace Yngir {
     public static class ArithmeticLogic {
         public static int Roll(this Entity Roller) {
             string Input = Roller.Attribute["Roll"];
