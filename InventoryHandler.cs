9831ed4985cea419af316b9e6e3e811525fd05b8
JamesonHolden <77400851+JamesonHolden@users.noreply.github.com>
2021-01-13 15:39:26 +0000

    Create InventoryHandler.cs

---
+++ InventoryHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unyschkovsky {
+    public class InventoryHandler {
+        public Dictionary<string, Weapon> Weapons = new Dictionary<string, Weapon>();
+
+    }
+}
