1b304b55d2a531d1faa1ce5dd89e9f0865a545a7
JamesonHolden <77400851+JamesonHolden@users.noreply.github.com>
2021-01-13 15:51:25 +0000

    Update Weapon.cs

--- Weapon.cs
+++ Weapon.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unyschkovsky {
+namespace Yngir {
     public class Weapon {
         public string Roll, Name, Quality;
         public bool Usable;
