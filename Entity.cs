2265a5e0e7dac57f3d6c4233ea5e4eb27ae4c9a8
JamesonHolden <77400851+JamesonHolden@users.noreply.github.com>
2021-01-13 15:38:51 +0000

    Create Entity.cs

---
+++ Entity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unyschkovsky {
+    public abstract class Entity {
+        public Dictionary<string, string> Attribute;
+        public Dictionary<string, int> Value;
+    }
+}
