e7ba5df4d5066474e7ba38195e58a6c0722a1855
JamesonHolden <77400851+JamesonHolden@users.noreply.github.com>
2021-01-13 15:40:34 +0000

    Create Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unyschkovsky {
+    public class Weapon {
+        public string Roll, Name, Quality;
+        public bool Usable;
+        public int Value;
+    }
+}
