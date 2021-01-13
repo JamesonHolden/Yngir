651c8748107f7ca4692553ff47293a7e1b182806
JamesonHolden <77400851+JamesonHolden@users.noreply.github.com>
2021-01-13 15:39:41 +0000

    Create ModularMenu.cs

---
+++ ModularMenu.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unyschkovsky {
+    public class ModularMenu<T> {
+        private bool Running;
+        private int OptionSelected;
+        public string Title;
+        public ConsoleColor SelectedColour;
+        public List<string> Articles;
+        public List<T> Actions;
+        public ModularMenu(List<string> Articles, List<T> Actions, string Title = "", ConsoleColor SelectedColour = ConsoleColor.Red) {
+            this.SelectedColour = SelectedColour;
+            this.Title = Title;
+            this.Articles = Articles;
+            this.Actions = Actions;
+        }
+        public void Open(out T output) {
+            output = Actions[0];
+            Running = true;
+            OptionSelected = 0;
+            while (Running) {
+                Console.Clear();
+                if (Title != "") ConsoleStream.PrintTitle(Title);
+                for (int Index = 0; Index < Articles.Count; Index++) {
+                    if (Index == OptionSelected) ConsoleStream.ColouredWriteLine($"> {Articles[Index]} <", SelectedColour);
+                    else Console.WriteLine(Articles[Index]);
+                }
+                ConsoleKey UserInput = ConsoleStream.ReadKey().Key;
+                if (UserInput == ConsoleKey.Enter) {
+                    output = Actions[OptionSelected];
+                    break;
+                }
+                else if (UserInput == ConsoleKey.S || UserInput == ConsoleKey.DownArrow) {
+                    if (OptionSelected < Articles.Count - 1) OptionSelected++;
+                    else OptionSelected = 0;
+                }
+                else if (UserInput == ConsoleKey.W || UserInput == ConsoleKey.UpArrow) {
+                    if (OptionSelected > 0) OptionSelected--;
+                    else OptionSelected = Articles.Count - 1;
+                }
+            }
+        }
+    }
+}
